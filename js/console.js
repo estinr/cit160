b2c5322cd89642451f885e934f86b56f57ecb86d
estinr <ereinsch@byui.edu>
2022-01-26 18:37:20 -0600

    draft

---
+++ js/console.js
@@ -0,0 +1,8 @@
+const words = `And it came to pass that the people of Sarah did till the land, and raise all manner of grain, and flocks of herds, and flocks of all manner of cattle of every kind, and flocks.`;
+
+const count = words.split(' ');
+
+const len = count;
+
+console.log(count)
+
