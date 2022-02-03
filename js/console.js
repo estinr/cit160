3b20b2940ec8f09fd89fe5f5d6d02a31a462694f
estinr <ereinsch@byui.edu>
2022-02-02 23:58:33 -0600

    portal/lbs2kilos

--- js/console.js
+++ js/console.js
@@ -1,8 +1,8 @@
-const words = `And it came to pass that the people of Sarah did till the land, and raise all manner of grain, and flocks of herds, and flocks of all manner of cattle of every kind, and flocks.`;
+// const words = `And it came to pass that the people of Sarah did till the land, and raise all manner of grain, and flocks of herds, and flocks of all manner of cattle of every kind, and flocks.`;
 
-const count = words.split(' ');
+// const count = words.split(' ');
 
-const len = count;
+// const len = count;
 
-console.log(count)
+// console.log(len)
 
