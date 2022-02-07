d6357bcf346f3e0dbeddd5aaf26a62ba844dfa18
estinr <ereinsch@byui.edu>
2022-02-06 18:26:12 -0600

    exam1

--- js/console.js
+++ js/console.js
@@ -1,15 +1,38 @@
-let e = m * c ** 2;
-let s = (a + b + c) / 2;
-let r = 2 * k + (6 - n) / (3 * p ** 2);
-z = z + x - 3 * y;
-b = b * q - s;
+// let e = m * c ** 2;
+// let s = (a + b + c) / 2;
+// let r = 2 * k + (6 - n) / (3 * p ** 2);
+// z = z + x - 3 * y;
+// b = b * q - s;
 
 //REVIEW SOLUTIONS
-let E = m * Math.pow(c, 2);
+// let E = m * Math.pow(c, 2);
 //or the following or related variations are OK
 
-var E = m * c * c /* or use (c ** 2) */;
-let s = (a + b + c) / 2;
-let r = 2 * k + (6 - n) / 3 * p * p;
-z = z + x - 3 * y;
-b = b * (q - s);
+// var E = m * c * c /* or use (c ** 2) */;
+// let s = (a + b + c) / 2;
+// let r = 2 * k + (6 - n) / 3 * p * p;
+// z = z + x - 3 * y;
+// b = b * (q - s);
+
+
+// let classStanding = 'J';
+// var registerDate;
+// switch (classStanding) {
+//     case 'F':
+//         registerDate = "Nov 23";
+//         break;
+//     case 'S':
+//         registerDate = "Nov 17";
+//         break;
+//     case 'J':
+//         registerDate = "Nov 12";
+//         break;
+//     case 'N':
+//         registerDate = "Nov 6";
+//         break;
+//     default:
+//         registerDate = "none";
+//         alert("unknown class standing");
+//         break;
+// }
+// document.getElementById('output').innerHTML = registerDate;
