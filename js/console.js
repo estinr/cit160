48a47fbe3f680458ea52b08bd1fdb06dde2fee3d
estinr <ereinsch@byui.edu>
2022-02-09 15:48:09 -0600

    weather

--- js/console.js
+++ js/console.js
@@ -1,38 +1,28 @@
-// let e = m * c ** 2;
-// let s = (a + b + c) / 2;
-// let r = 2 * k + (6 - n) / (3 * p ** 2);
-// z = z + x - 3 * y;
-// b = b * q - s;
+function ticketPrice() {
+    var age = 38;
+    var gamesAttended = 6;
+    
+    var price = 0;
+    if (age < 18) {
+        if (gamesAttended < 6)
+            price = 8.0;
+        else if (gamesAttended < 11)
+            price = 6.0;
+        else
+            price = 5.0;
+    }
+    else if (age < 55) {
+        if (gamesAttended < 11)
+            price = 10.0;
+        else
+            price = 8.0;
+    }
+    else {
+        if (gamesAttended < 11)
+            price = 8.0;
+        else
+            price = 6.0;
+    }
 
-//REVIEW SOLUTIONS
-// let E = m * Math.pow(c, 2);
-//or the following or related variations are OK
-
-// var E = m * c * c /* or use (c ** 2) */;
-// let s = (a + b + c) / 2;
-// let r = 2 * k + (6 - n) / 3 * p * p;
-// z = z + x - 3 * y;
-// b = b * (q - s);
-
-
-// let classStanding = 'J';
-// var registerDate;
-// switch (classStanding) {
-//     case 'F':
-//         registerDate = "Nov 23";
-//         break;
-//     case 'S':
-//         registerDate = "Nov 17";
-//         break;
-//     case 'J':
-//         registerDate = "Nov 12";
-//         break;
-//     case 'N':
-//         registerDate = "Nov 6";
-//         break;
-//     default:
-//         registerDate = "none";
-//         alert("unknown class standing");
-//         break;
-// }
-// document.getElementById('output').innerHTML = registerDate;
+    console.log(price);
+}
