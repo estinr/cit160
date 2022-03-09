22408b69a50f42b0aecc8dd149c124f145194a4c
estinr <ereinsch@byui.edu>
2022-03-09 15:27:57 -0600

    discount_funct-index

--- js/console.js
+++ js/console.js
@@ -1,28 +1,5 @@
-function ticketPrice() {
-    var age = 38;
-    var gamesAttended = 6;
-    
-    var price = 0;
-    if (age < 18) {
-        if (gamesAttended < 6)
-            price = 8.0;
-        else if (gamesAttended < 11)
-            price = 6.0;
-        else
-            price = 5.0;
-    }
-    else if (age < 55) {
-        if (gamesAttended < 11)
-            price = 10.0;
-        else
-            price = 8.0;
-    }
-    else {
-        if (gamesAttended < 11)
-            price = 8.0;
-        else
-            price = 6.0;
-    }
-
-    console.log(price);
+function logKey(event) {
+    console.log(`You pressed "${event.key}".`);
 }
+
+textBox.addEventListener('keydown', logKey);
